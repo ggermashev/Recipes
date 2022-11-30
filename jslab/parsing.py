b09bed020759df6d00cbe9d7d24aca23889eac48
ggermashev <ggermashev@gmail.com>
2022-11-30 23:29:11 +0300

    parsing

--- jslab/parsing.py
+++ jslab/parsing.py
@@ -1,13 +1,21 @@
+import time
+
 from bs4 import BeautifulSoup
 import requests
 import sqlite3
 import datetime
-
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from webdriver_manager.chrome import ChromeDriverManager
 
 def get_recepts():
     conn = sqlite3.connect('db.sqlite3')
     cur = conn.cursor()
+    driver = webdriver.Chrome(ChromeDriverManager().install())
+    link = 'https://www.russianfood.com/recipes/'
+    driver.get(link)
+    window_before = driver.current_window_handle
+    hrefs = driver.find_elements(By.XPATH, "//dd/a[contains(@href,'/recipes/bytype')]")
+    for h in hrefs:
+        print(h.text)
 
-
-
-get_recepts()
